66b13d8157f71122c07cc3d91ee2232f64d2e367
Jiyu Jang <gujiyujang@gmail.com>
2024-01-09 00:24:19 +0900

    Update 소수 찾기.js

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -32,7 +32,7 @@ function solution(numbers) {
         if(num === 2) return true;
         if(num <= 1 || num % 2 === 0) return false;
         
-        for (let i = 3; i * i <= num; i += 2) {
+        for (let i = 3; i * i <= num; i += 2) {  // 절반보다 큰 수로는 나눠볼 필요가 없음
             if(num % i === 0) return false;
         } 
         
