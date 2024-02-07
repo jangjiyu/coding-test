df52e5f12d7be1ca7b1e1cbcc2a1b1a00b31327d
Jiyu Jang <gujiyujang@gmail.com>
2024-02-08 01:49:34 +0900

    [level 2] Title: 전화번호 목록, Time: 113.98 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,26 +1,9 @@
 function solution(phone_book) {
-    let answer = true;
-    
-    phone_book.sort((a, b) => b - a);
-    
-    const minLen = phone_book[phone_book.length - 1].length;
-    
-    const obj = {};
-    
-    for(let i = 0; i < phone_book.length; i++) {
-        for(let j = minLen; j < phone_book[i].length; j++) {
-            obj[phone_book[i].slice(0, j)] = true;
-        }
-    }
+    phone_book.sort();
 
-    while(phone_book.length) {
-        if(obj[phone_book[phone_book.length - 1]]) {
-            answer = false;
-            break;
-        }
-            
-        phone_book.pop();
+    for(let i = 0; i < phone_book.length - 1; i++) {
+        if(phone_book[i] === phone_book[i + 1].slice(0, phone_book[i].length)) return false;
     }
     
-    return answer;
+    return true;
 }
