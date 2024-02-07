630eed08d7ca6145b355ef272bf5ea0420cab2b5
Jiyu Jang <gujiyujang@gmail.com>
2024-02-08 01:14:49 +0900

    [level 2] Title: 전화번호 목록, Time: 858.34 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,24 +1,51 @@
+// function solution(phone_book) {
+//     let answer = true;
+    
+//     // phone_book sort
+//     phone_book.sort((a, b) => a - b);
+    
+//     const minLen = phone_book[0].length;
+    
+//     const obj = {};
+    
+//     for(let i = 0; i < phone_book.length; i++) {
+//         for(let j = minLen; j < phone_book[i].length; j++) {
+//             obj[phone_book[i].slice(0, j)] = i;
+//         }
+//     }
+
+//     for(let i = 0; i < phone_book.length; i++) {
+//         if(obj[phone_book[i]] && obj[phone_book[i]] !== i) {
+//             answer = false;
+//             break;
+//         }
+//     }
+    
+//     return answer;
+// }
 function solution(phone_book) {
     let answer = true;
     
     // phone_book sort
-    phone_book.sort((a, b) => a - b);
+    phone_book.sort((a, b) => b - a);
     
-    const minLen = phone_book[0].length;
+    const minLen = phone_book[phone_book.length - 1].length;
     
     const obj = {};
     
     for(let i = 0; i < phone_book.length; i++) {
         for(let j = minLen; j < phone_book[i].length; j++) {
-            obj[phone_book[i].slice(0, j)] = i;
+            obj[phone_book[i].slice(0, j)] = true;
         }
     }
 
-    for(let i = 0; i < phone_book.length; i++) {
-        if(obj[phone_book[i]] && obj[phone_book[i]] !== i) {
+    for(let i = phone_book.length - 1; i >= 0; i--) {
+        if(obj[phone_book[i]]) {
             answer = false;
             break;
         }
+            
+        phone_book.pop();
     }
     
     return answer;
