2dd5180bd3bd76fd7318af60c2554f42f03a19af
Jiyu Jang <gujiyujang@gmail.com>
2024-10-14 17:01:44 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,17 +1,12 @@
 function solution(clothes) {
   let answer = 0;
-  let obj = {};
+  const clothesObj = {};
 
-  clothes.forEach((cloth) => {
-    obj[cloth[1]] = obj[cloth[1]] ? obj[cloth[1]] + 1 : 1;
+  clothes.forEach(([name, type]) => {
+    clothesObj[type] = clothesObj[type] ? clothesObj[type] + 1 : 1;
   });
 
-  // 경우의 수 계산
-  // { a: 1, b: 1, c: 1 }
-  // (1 + 1) * (1 + 1) * (1 + 1) - 1
-  // { a: 3 }
-  // (3 + 1) - 1
-  answer = Object.values(obj).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
+  answer = Object.values(clothesObj).reduce((acc, cur) => acc * (cur + 1), 1);
 
-  return answer;
+  return answer - 1;
 }
